4891c443efd41a7116bbdfc9fded9fb284c3c072
Mohammad Azimi <mmaz13841384@gmail.com>
2025-07-08 08:25:07 +0330

    Working for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
- base: '/MHMH.dev-personal-page-/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
