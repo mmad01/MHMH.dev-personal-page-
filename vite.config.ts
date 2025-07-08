c6c52a1fabde23a9cb99ae2575df4883945b1625
Mohammad Azimi <mmaz13841384@gmail.com>
2025-07-08 08:20:21 +0330

    fix vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+ base: '/MHMH.dev-personal-page-/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
